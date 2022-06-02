a89b62803cfbd235b15cc6297154ea1ba7f21952
Michael Geere <94830893+michaelgeere@users.noreply.github.com>
2022-06-02 17:28:57 +0200

    (php) Added front-end for login and createUser

---
+++ Front-end/createUser.php
@@ -0,0 +1,9 @@
+<form action="createUser-validation.php" method="POST">
+<label for="user">Username:</label><br>
+  <input type="text" id="user" name="username"><br>
+  <label for="pass">Password:</label><br>
+  <input type="text" id="pass" name="password"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+</html>
