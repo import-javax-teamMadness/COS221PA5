dd26cd72fc5b7d800ecaab88c4f5e2524216bc0a
MadKea <98401898+MadKea@users.noreply.github.com>
2022-06-04 01:27:08 +0200

    Add files via upload

---
+++ (php)UserManagement/header.php
@@ -0,0 +1,17 @@
+<?php
+
+$header= '
+
+    
+<a href="#" class="btn">Home</a>
+<a href="#" class="btn">Schedule</a>
+<a href="#" class="btn">Players</a>
+<a href="#" class="btn">Results</a>
+<a href="#" class="btn">Media</a>
+<a href="#" class="btn">Score Capturing</a>';
+
+echo $header;
+?>
+
+
+<!--<a href="https://twitter.com/freshcontrast" target="_blank" class="footer">@FRESHCONTRAST</a>-->
