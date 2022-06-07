64fe675602afccbc88489bca1509dd51c9268d0e
careenandrew <105283247+careenandrew@users.noreply.github.com>
2022-06-07 17:47:56 +0200

    Add files via upload

---
+++ Header/AddTournament.php
@@ -0,0 +1,28 @@
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+  
+<?php include_once('header.php'); ?>
+    
+<div class="controller">
+    <h1>AddTournament in php</h1>
+</div>
+
+</body>
+</html>
---
+++ Header/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul class="nav_links">
+            <li><a  href="./index.php">Home</a>
+            <li><a  href="./Login.php">Login</a>
+            <li><a  href="./AddPlayer.php">AddPlayer</a>
+            <li><a  href="./AddTournament.php">AddTournament</a>
+            <li><a  href="./AddUser.php">AddUser</a>
+            <li><a  href="./ManageUser.php">ManageUser</a>
+            <li><a  href="./Players.php">Players</a>
+            <li><a  href="./Tournaments.php">Tournaments</a>
+        </ul>
+    </nav>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
